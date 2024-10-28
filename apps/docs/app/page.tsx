15e465716fdf30c6b7f996d1683c7bae8e55bc37
vimulatus <vasu.mit2501@gmail.com>
2024-10-28 17:11:26 +0530

    style: linting w/ biome

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="bg-red-500">Docs app</div>;
+	return <div className="bg-red-500">Docs app</div>;
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,9 @@
 import { UiTest } from "@repo/ui/ui-test";
 
 export default function Home() {
-  return (
-    <div className="bg-blue-500">
-      Web app <UiTest />
-    </div>
-  );
+	return (
+		<div className="bg-blue-500">
+			Web app <UiTest />
+		</div>
+	);
 }
